65593809c341182c0cc88993003ae274d08cd6e7
nouyang <nouyang@email>
2018-06-30 21:24:40 -0400

    added favicons, background image, removed nav header

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object('config')
 #db = SQLAlchemy(app)
 
@@ -46,27 +46,27 @@ def home():
     return render_template('pages/placeholder.home.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
+# @app.route('/about')
+# def about():
+    # return render_template('pages/placeholder.about.html')
 
 
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
+# @app.route('/login')
+# def login():
+    # form = LoginForm(request.form)
+    # return render_template('forms/login.html', form=form)
 
 
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
+# @app.route('/register')
+# def register():
+    # form = RegisterForm(request.form)
+    # return render_template('forms/register.html', form=form)
 
 
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
+# @app.route('/forgot')
+# def forgot():
+    # form = ForgotForm(request.form)
+    # return render_template('forms/forgot.html', form=form)
 
 # Error handlers.
 
